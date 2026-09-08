1fac6bdfc192d63e04f3bf86c72134ac6dbe9ef7
norhan-elsayed <ahmedelsayednn9@gmail.com>
2026-09-08 19:09:12 +0300

    task 3

---
+++ task3_api_ecommerse/bin/task3_api_ecommerse.dart
@@ -0,0 +1,5 @@
+import 'package:task3_api_ecommerse/task3_api_ecommerse.dart' as task3_api_ecommerse;
+
+void main(List<String> arguments) {
+  print('Hello world: ${task3_api_ecommerse.calculate()}!');
+}
---
+++ task3_api_ecommerse/test/task3_api_ecommerse_test.dart
@@ -0,0 +1,8 @@
+import 'package:task3_api_ecommerse/task3_api_ecommerse.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
