3abf3c5540ae49890d96357c1622013a609c1381
norhan-elsayed <ahmedelsayednn9@gmail.com>
2026-09-09 23:24:41 +0300

    --

--- task2_todo_design/lib/main.dart
+++ task2_todo_design/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:task2_todo_design/screens/add_task_screen.dart';
