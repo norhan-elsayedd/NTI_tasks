2f44d806eb12e30b719cbbf313c58ffd5dff2cf1
norhan-elsayed <ahmedelsayednn9@gmail.com>
2026-09-09 14:24:54 +0300

    task 2

---
+++ task2_todo_design/lib/core/utilies/app_assests.dart
@@ -0,0 +1,14 @@
+abstract class AppImages {
+ static final String flag ="assets/images/flag.png";
+ static final String home ="assets/images/home.png";
+ static final String personal ="assets/images/personal.png";
+ static final String work ="assets/images/work.png";
+}
+
+abstract class AppSvgs{
+  static final String logo ="assets/images/logo.svg";
+  static final String profile ="assets/images/Profile.svg";
+   static final String Password ="assets/images/Password.svg";
+  
+
+}
---
+++ task2_todo_design/lib/core/utilies/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+abstract class AppColors {
+  static const Color primary =Color(0xFF149954);
+  static const Color background=Color(0xFFF3F5F4);
+}
